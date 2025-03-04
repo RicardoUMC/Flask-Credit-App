57eee59d8a1fd50609d4af7a773c6f1a49a9cc2b
Ricardo Mora <rurimoca@gmail.com>
2025-03-03 23:37:14 -0600

    Added SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-import sqlite3
-from flask impor Flask, render_tamble, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///creditos.db'
+db = SQLAlchemy(app)
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    return "todo"
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 8080)
